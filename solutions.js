ff8b7463353916989bc227f1b2a1f1fe144972ae
Bavon101 <bavonfate@gmail.com>
2021-11-30 22:24:00 +0300

    challenge #2

--- solutions.js
+++ solutions.js
@@ -15,8 +15,8 @@ function breakingRecords(scores) {
 			lowest++;
 		}
 		sorted.push(scores[i]);
-		max = Math.max(sorted);
-		min = Math.min(sorted);
+		max = Math.max.apply(null,sorted);
+		min = Math.min.apply(null,sorted);
 		
 		
 
@@ -28,4 +28,22 @@ function breakingRecords(scores) {
 
 }
 
-console.log(breakingRecords([3, 4 ,21 ,36 ,10 ,28 ,35, 5 ,24, 42]));
+console.log(breakingRecords([3, 4, 21, 36, 10, 28, 35, 5, 24, 42]));
+
+let alphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+function designerPdfViewer(h, word) {
+	// Write your code here
+	let wordHeight = [];
+	for (let i = 0; i < word.length; i++){
+		let index = alphabets.indexOf(word[i]);
+		wordHeight.push(h[index]);
+		
+	}
+	let height = Math.max.apply(null,wordHeight);
+
+	return (height * word.length);
+
+}
+
+
+console.log(designerPdfViewer([1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7], "zaba"));
